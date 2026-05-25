1f8fe8bbd9a5c7b3faa9f183681e7f341ac07596
Jenny Anne Dula <annedula26@gmail.com>
2026-05-25 17:55:18 +0000

    src/core/__pycache__/settings.cpython-312.pyc

--- src/system/audio_system.py
+++ src/system/audio_system.py
@@ -1,26 +1,33 @@
 import pygame
 
+
 def init_audio():
     pygame.mixer.init()
     pygame.mixer.music.load('src/assets/during game music.mp3')
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play(-1)
 
+
 def play_move_sound():
     pygame.mixer.Sound('src/assets/carmoves.wav').play()
+
 
 def play_dog_hit_sound():
     pygame.mixer.Sound('src/assets/dog.mp3').play()
 
+
 def play_cat_hit_sound():
     pygame.mixer.Sound('src/assets/cat.mp3').play()
 
-def play_bato_hit_sound():                                
+
+def play_bato_hit_sound():
     pygame.mixer.Sound('src/assets/bato.mp3').play()
+
 
 def play_game_over_music():
     pygame.mixer.music.load('src/assets/weak.mp3')
     pygame.mixer.music.play(-1)
+
 
 def stop_game_over_music():
     pygame.mixer.music.stop()
